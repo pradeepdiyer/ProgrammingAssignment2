3643f87dec7e6dfae4bad565cf78aa3ed80a1fac
pradeepdiyer <pradeepdiyer@gmail.com>
2015-05-22 20:49:36 -0700

    Solution placed in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix can set and get the value of a matrix created in it's parent environment.
+## It can also set and get the value of the inverse of that matrix.
+## cacheSolve returns the inverse of the special "matrix" from above by either computing it or retrieving it from cache
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(matrixInverse) inv <<- matrixInverse
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the special "matrix" from above by either computing it or retrieving it from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
